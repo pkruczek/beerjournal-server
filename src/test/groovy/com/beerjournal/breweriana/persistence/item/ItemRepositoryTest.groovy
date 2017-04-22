ac15cd4bd7f30481341b90394bc629b7574ac781
Grzegorz Laganowski <g.laganowski94@gmail.com>
2017-04-22 11:16:28 +0200

    Added test for findAllNotInUserCollection method

--- src/test/groovy/com/beerjournal/breweriana/persistence/item/ItemRepositoryTest.groovy
+++ src/test/groovy/com/beerjournal/breweriana/persistence/item/ItemRepositoryTest.groovy
@@ -1,9 +1,12 @@
 package com.beerjournal.breweriana.persistence.item
 
+import com.beerjournal.breweriana.persistence.UserCollectionRepository
+import com.beerjournal.breweriana.persistence.UserRepository
 import com.beerjournal.breweriana.persistence.category.Category
 import com.beerjournal.breweriana.persistence.CategoryCrudRepository
 import com.beerjournal.breweriana.persistence.ItemCrudRepository
 import com.beerjournal.breweriana.persistence.ItemRepository
+import com.beerjournal.breweriana.persistence.collection.UserCollection
 import com.beerjournal.breweriana.utils.TestUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -25,29 +28,70 @@ class ItemRepositoryTest extends Specification {
     CategoryCrudRepository categoryRepository
 
     @Autowired
+    UserCollectionRepository userCollectionRepository
+
+    @Autowired
+    UserRepository userRepository
+
+    @Autowired
     MongoTemplate mongoTemplate
 
+    def savedUser
+    def someUserCollection
+
     def setup() {
+        savedUser = userRepository.save(TestUtils.someUser())
+        someUserCollection = UserCollection.builder()
+                .ownerId(savedUser.id)
+                .build()
         mongoTemplate.db.dropDatabase()
     }
 
-    def someItem = TestUtils.someItem()
+    def someItem1 = TestUtils.someItem()
+    def someItem2 = TestUtils.someItem()
 
     def "should save an item"() {
         when:
-        itemRepository.save(someItem)
+        itemRepository.save(someItem1)
 
         then:
-        def maybeItem = itemCrudRepository.findOneByName(someItem.name)
-        TestUtils.equalsOptionalValue(maybeItem, someItem)
+        def maybeItem = itemCrudRepository.findOneByName(someItem1.name)
+        TestUtils.equalsOptionalValue(maybeItem, someItem1)
     }
 
     def "should add category if not exists"() {
         when:
-        itemRepository.save(someItem)
+        itemRepository.save(someItem1)
 
         then:
-        def maybeCategory = categoryRepository.findOneByName(someItem.category)
-        TestUtils.equalsOptionalValue(maybeCategory, Category.of(someItem.category))
+        def maybeCategory = categoryRepository.findOneByName(someItem1.category)
+        TestUtils.equalsOptionalValue(maybeCategory, Category.of(someItem1.category))
     }
+
+    def "should return not owned item"() {
+        given:
+        userCollectionRepository.save(someUserCollection)
+        itemRepository.save(someItem1)
+        itemRepository.save(someItem2)
+
+        when:
+        def missingItems = itemRepository.findAllNotInUserCollection(savedUser.id);
+
+        then:
+        missingItems.containsAll(Arrays.asList(someItem1, someItem2))
+    }
+
+    def "shouldn't return owned item"() {
+        given:
+        userCollectionRepository.save(someUserCollection)
+        itemRepository.save(someItem1)
+        userCollectionRepository.addNewItem(savedUser.id, someItem1)
+
+        when:
+        def missingItems = itemRepository.findAllNotInUserCollection(savedUser.id);
+
+        then:
+        !missingItems.contains(someItem1)
+    }
+
 }
