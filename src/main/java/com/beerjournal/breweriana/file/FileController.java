82702cd3baa3454b505a72da9ad1665b0f75fc17
Grzegorz Laganowski <g.laganowski94@gmail.com>
2017-05-07 13:05:06 +0200

    Base64 file encode

--- src/main/java/com/beerjournal/breweriana/file/FileController.java
+++ src/main/java/com/beerjournal/breweriana/file/FileController.java
@@ -1,14 +1,17 @@
 package com.beerjournal.breweriana.file;
 
+import com.google.common.io.ByteStreams;
 import com.mongodb.gridfs.GridFSDBFile;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/api")
@@ -18,12 +21,12 @@ public class FileController {
     private final FileService fileService;
 
     @GetMapping("files/{fileId}")
-    public ResponseEntity<InputStreamResource> getImageItem(@PathVariable("fileId") String fileId) {
+    public ResponseEntity<byte[]> getImageItem(@PathVariable("fileId") String fileId) throws IOException {
         GridFSDBFile file = fileService.getFile(fileId);
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.parseMediaType((file.getContentType())))
-                .body(new InputStreamResource(file.getInputStream()));
+                .body(Base64.getEncoder().encode(ByteStreams.toByteArray(file.getInputStream())));
     }
 
 }
