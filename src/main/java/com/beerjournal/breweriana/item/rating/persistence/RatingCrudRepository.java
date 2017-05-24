efd2ececf6a085d04bfb341163f8d56fa2af49e9
Tymoteusz Machowski <tmachows@gmail.com>
2017-05-25 00:27:22 +0200

    delegate queries to spring data

--- src/main/java/com/beerjournal/breweriana/item/rating/persistence/RatingCrudRepository.java
+++ src/main/java/com/beerjournal/breweriana/item/rating/persistence/RatingCrudRepository.java
@@ -4,7 +4,14 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface RatingCrudRepository extends MongoRepository<Rating, ObjectId> {
     Optional<Rating> findOneById(ObjectId id);
+
+    Set<Rating> findAllByItemId(ObjectId id);
+
+    Set<Rating> findAllByRaterId(ObjectId id);
+
+    Optional<Rating> findOneByItemIdAndRaterId(ObjectId itemId, ObjectId raterId);
 }
--- src/main/java/com/beerjournal/breweriana/item/rating/persistence/RatingRepository.java
+++ src/main/java/com/beerjournal/breweriana/item/rating/persistence/RatingRepository.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,21 +20,18 @@ public class RatingRepository {
     private final ItemRepository itemRepository;
 
     public Set<Rating> findRatingsByItemId(ObjectId itemId) {
-        return ratingCrudRepository.findAll().stream()
-                .filter(rating -> rating.getItemId().equals(itemId))
-                .collect(Collectors.toSet());
+        return ratingCrudRepository.findAllByItemId(itemId);
     }
 
     public Set<Rating> findRatingsByRaterId(ObjectId raterId) {
-        return ratingCrudRepository.findAll().stream()
-                .filter(rating -> rating.getRaterId().equals(raterId))
-                .collect(Collectors.toSet());
+        return ratingCrudRepository.findAllByRaterId(raterId);
     }
 
     public Rating save(Rating rating) {
         validateRating(rating);
 
-        Optional<Rating> existingRating = findRating(rating.getItemId(), rating.getRaterId());
+        Optional<Rating> existingRating = ratingCrudRepository
+                .findOneByItemIdAndRaterId(rating.getItemId(), rating.getRaterId());
         existingRating.ifPresent(ratingCrudRepository::delete);
 
         return ratingCrudRepository.save(rating);
@@ -46,12 +42,6 @@ public class RatingRepository {
                 .orElseThrow(() -> new BeerJournalException(ErrorInfo.USER_NOT_FOUND));
         itemRepository.findOneById(rating.getItemId())
                 .orElseThrow(() -> new BeerJournalException(ErrorInfo.ITEM_NOT_FOUND));
-    }
-
-    private Optional<Rating> findRating(ObjectId itemId, ObjectId raterId) {
-        return findRatingsByItemId(itemId).stream()
-                .filter(rating -> rating.getRaterId().equals(raterId))
-                .findAny();
     }
 
     public Rating delete(ObjectId ratingId) {
