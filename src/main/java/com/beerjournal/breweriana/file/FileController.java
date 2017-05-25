4bac651cfc7637a415586d718465baae2f02d459
Paweł Ćwik <cwikpawel@o2.pl>
2017-05-25 14:05:47 +0200

    send cache headers

--- src/main/java/com/beerjournal/breweriana/file/FileController.java
+++ src/main/java/com/beerjournal/breweriana/file/FileController.java
@@ -3,12 +3,15 @@ package com.beerjournal.breweriana.file;
 import com.mongodb.gridfs.GridFSDBFile;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.concurrent.TimeUnit.DAYS;
 
 @RestController
 @RequestMapping("/api")
@@ -22,6 +25,7 @@ public class FileController {
         GridFSDBFile file = fileService.getFile(fileId);
         return ResponseEntity
                 .ok()
+                .cacheControl(CacheControl.maxAge(365, DAYS))
                 .contentType(MediaType.parseMediaType((file.getContentType())))
                 .body(new InputStreamResource(file.getInputStream()));
     }
